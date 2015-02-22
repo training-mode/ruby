879eb9c7ed0c60bdcee834c3642e4425248aa393
Matt Palen <matthewmpalen@gmail.com>
2015-02-22 00:28:13 -0500

    Assignment

---
+++ intro/4_data_structures/queen.rb
@@ -0,0 +1,9 @@
+# A little bit of classic rock
+
+lyrics = "Is this the real life?"\
+         "Is this just fantasy?"\
+         "Caught in a landslide,"\
+         "No escape from reality."
+
+# Your code goes here
+
