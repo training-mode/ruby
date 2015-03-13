8205c75aa01ab025253383edf149a27df74f590a
Matt Palen <matthewmpalen@gmail.com>
2015-03-13 10:27:43 -0400

    Expanded assignment

--- intro/8_procs_lambdas/towers.rb
+++ intro/8_procs_lambdas/towers.rb
@@ -1,1 +1,16 @@
+# Height in meters
+towers = [
+    { :name => "Berliner Fernsehturm", :height => 368.0 }, 
+    { :name => "Canton Tower", :height => 600.0 }, 
+    { :name => "Eiffel Tower", :height => 324.0 }, 
+    { :name => "Tokyo Tower", :height => 332.6 },  
+    { :name => "Stratosphere Tower", :height => 350.2 }, 
+]
+
+factor = 100/2.54   # Converts centimeters to inches
+
+conversion = lambda do |x|
+    # Your code goes here
+end
+
 # Your code goes here
