d036439f54b38b99ce443fb7cf006aab11949870
Harold Random <haroldrandom@HarolddeMacBook-Pro.local>
2016-12-11 13:47:36 +0800

    finish 04_data_structures

--- intro/04_data_structures/queen.rb
+++ intro/04_data_structures/queen.rb
@@ -6,4 +6,16 @@ lyrics = "Is this the real life?"\
          "No escape from reality."
 
 # Your code goes here
+marks = Hash.new(0)
 
+(0...lyrics.length ).each { |i|
+    c = lyrics[i]
+    
+    next if c == ' '
+
+    marks[c] += 1
+}
+
+for k, v in marks
+    puts "character:#{k} - count:#{v}"
+end
