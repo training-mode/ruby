33fe2cc69d9a923cff864fe33290ec14912bbc41
harold <xxx@gmail.com>
2016-12-12 15:23:34 +0800

    08_procs_lambdas

--- intro/08_procs_lambdas/towers.rb
+++ intro/08_procs_lambdas/towers.rb
@@ -11,6 +11,18 @@ factor = 100/2.54   # Converts centimeters to inches
 
 conversion = lambda do |x|
   # Your code goes here
+    for item in x
+        converted_height = sprintf('%.1f', item[:height] / factor)
+        puts "%-25s => %s" % [item[:name], converted_height]
+    end
 end
 
+
 # Your code goes here
+sorted_array = towers.sort do |a, b|
+    b[:height] <=> a[:height]
+end
+
+
+conversion.call(sorted_array)
+
