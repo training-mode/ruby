41a59a494664cc79d5137edf08b190a1909ff521
fredosantana <santana.wil@gmail.com>
2017-03-14 22:17:48 -0400

    Solution to names.rb exercise

--- intro/02_control_flow/names.rb
+++ intro/02_control_flow/names.rb
@@ -5,4 +5,14 @@ name3 = "Beric Dondarrion"
 name4 = "Melisandre"
 
 # Your code goes here
+avg_length = (name1.length + name2.length + name3.length + name4.length) / 4
+print "What is your name?"
+my_name = gets.chomp
+
+
+if avg_length >= my_name.length
+  print "#{my_name} is shorter than average"
+else
+  print "#{my_name} is shorter than average"
+end
 
