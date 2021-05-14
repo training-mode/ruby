7888767adfdb3c6c4fcedf9567a9e314efd32767
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-05-14 16:57:00 +0100

    edited names.rb

--- intro/02_control_flow/names.rb
+++ intro/02_control_flow/names.rb
@@ -6,3 +6,12 @@ name4 = "Melisandre"
 
 # Your code goes here
 
+avg_length = (name1.length + name2.length + name3.length + name4.length)/ 4
+puts "What is your name"
+my_name = gets
+
+if (my_name.length < avg_length.size)
+    puts "#{my_name} is shorter than average"
+else
+    puts "#{my_name} is longer than average"
+end
