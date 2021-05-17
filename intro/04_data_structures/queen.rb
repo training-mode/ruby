65f680d3d9cc66241a68e552828657e49a947ceb
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-05-17 10:04:22 +0100

    edited queen.rb

--- intro/04_data_structures/queen.rb
+++ intro/04_data_structures/queen.rb
@@ -1,9 +1,6 @@
 # A little bit of classic rock
 
-lyrics = "Is this the real life?"\
-         "Is this just fantasy?"\
-         "Caught in a landslide,"\
-         "No escape from reality."
+lyrics = "Is this the real life?\nIs this just fantasy?\nCaught in a landslide,\nNo escape from reality."
 
 # Your code goes here
-
+puts lyrics.each_char.tally #=> {"I"=>2, "s"=>8, " "=>13, "t"=>7, "h"=>4, "i"=>6, "e"=>7, "r"=>3, "a"=>8, "l"=>5, "f"=>3, "?"=>2, "\n"=>3, "j"=>1, "u"=>2, "n"=>3, "y"=>2, "C"=>1, "g"=>1, "d"=>2, ","=>1, "N"=>1, "o"=>2, "c"=>1, "p"=>1, "m"=>1, "."=>1}
